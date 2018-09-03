631801e80048c012855a083231fd873130101823
Jordi Escudero <jordi.escudero@gmail.com>
2018-09-03 14:36:12 +0200

    first commit

---
+++ truffle.js
@@ -0,0 +1,28 @@
+
+require('dotenv').config()
+
+var Web3 = require('web3');
+
+console.log(process.env.DEVELOPMENT_ACCOUNT);
+
+module.exports = {
+  networks: {
+   development: {
+      host: process.env.DEVELOPMENT_HOST,
+      port: process.env.DEVELOPMENT_PORT, 
+      network_id: "*", 
+      gasPrice: 0,
+      gas: 4500000,
+      from: process.env.DEVELOPMENT_ACCOUNT,
+    },
+    alastria: {
+      provider: () => {
+        return new Web3.providers.HttpProvider(process.env.ALASTRIA_URL, 0, process.env.ALASTRIA_USER, process.env.ALASTRIA_PASSWORD);
+      },
+      network_id: "*", 
+      gasPrice: 0,
+      gas: 4500000,
+      from: process.env.ALASTRIA_ACCOUNT,
+    }
+  }
+};

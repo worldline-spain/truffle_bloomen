1c18af4b7182a1b22c49e3162bde76a7a7c30479
Daniel Bautista <daniel.bautista@worldline.com>
2018-09-06 16:14:29 +0200

    BloomenIndex - getCoins() and getMusicShops()

--- contracts/BloomenIndex.sol
+++ contracts/BloomenIndex.sol
@@ -2,14 +2,18 @@ pragma solidity ^0.4.22;
 
 import "./MusicShop.sol";
 import "./Coin.sol";
+import "./Strings.sol";
 
 contract BloomenIndex {
     
+    using Strings for *;
+
     //@dev Mappings do not store their keys, only the value which is stored at the state memory address calculated by a sha3 hash of the key itself. Any lookup into a mapping has to provide that original key or be able to calculate it.
     mapping (address => string) musicShopsMap;
     address[] musicShops;
     mapping (address => string) coinsMap;
     address[] coins;
+    string constant separator = "-";
 
     // @dev BloomenIndex.deployed().then(function(instance){return instance.createMusicShop("Discos WL", "0x3b4c4ae5194a7134ab182ccfbd9fd3605dd54ecb")}); The second parameter must be a valid Coin address.
     function createMusicShop(string _name, address _token) public {
@@ -27,20 +31,34 @@ contract BloomenIndex {
 
     // @dev BloomenIndex.deployed().then(function(instance){return instance.getMusicShops()});
     // @dev to parse de returned data: https://medium.com/coinmonks/solidity-tutorial-returning-structs-from-public-functions-e78e48efb378
-    function getMusicShops() public view returns (address[], string[]) {
-        string[] memory names = new string[](musicShops.length);
+    //                                 https://github.com/Arachnid/solidity-stringutils
+    function getMusicShops() public view returns (address[], string) {
+        string memory names;
         for (uint i = 0; i < musicShops.length; i++) {
-            names[i] = musicShopsMap[musicShops[i]];
+            string memory part;
+            if (i != 0) {
+                part = separator.toSlice().concat(musicShopsMap[musicShops[i]].toSlice());
+            } else {
+                part = musicShopsMap[musicShops[i]];
+            }
+            names = names.toSlice().concat(part.toSlice());
         }
         return (musicShops, names);
     }
 
     // @dev BloomenIndex.deployed().then(function(instance){return instance.getCoins()});
     // @dev to parse de returned data: https://medium.com/coinmonks/solidity-tutorial-returning-structs-from-public-functions-e78e48efb378
-    function getCoins() public view returns (address[], string[]) {
-        string[] memory names = new string[](coins.length);
+    //                                 https://github.com/Arachnid/solidity-stringutils
+    function getCoins() public view returns (address[], string) {
+        string memory names;
         for (uint i = 0; i < coins.length; i++) {
-            names[i] = coinsMap[coins[i]];
+            string memory part;
+            if (i != 0) {
+                part = separator.toSlice().concat(coinsMap[coins[i]].toSlice());
+            } else {
+                part = coinsMap[coins[i]];
+            }
+            names = names.toSlice().concat(part.toSlice());
         }
         return (coins, names);
     }
